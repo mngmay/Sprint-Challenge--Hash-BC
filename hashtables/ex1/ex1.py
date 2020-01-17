6547da9c7265f76fdd79d34cd7032ccd0b1fcb67
May Ng <mngmay@gmail.com>
2020-01-17 11:08:21 -0800

    refactored ex1. tests working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,28 +1,22 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        )
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-    print("WEIGHTS", weights)
 
     for i, weight in enumerate(weights):
-        hash_table_insert(ht, weight, i)
         item1 = hash_table_retrieve(ht, limit - weight)
         if item1 is not None:
-            print(item1, "NOT NONE!", weight)
-            item2 = hash_table_retrieve(ht, weight)
-            print(item1, item2)
+            item2 = i
             if item1 > item2:
                 return (item1, item2)
-            if item1 == item2:
-                return (1, 0)
             else:
                 return (item2, item1)
+        hash_table_insert(ht, weight, i)
     return None
 
 

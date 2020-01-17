1b34717b9afe974d4346c1f4f4e8f8554210b198
May Ng <mngmay@gmail.com>
2020-01-17 10:40:31 -0800

    hacky solution to ex1 :) need to deal with when item1 and item2 are the same and return the same indicies.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,11 +8,21 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    print("WEIGHTS", weights)
 
-    """
-    YOUR CODE HERE
-    """
-
+    for i, weight in enumerate(weights):
+        hash_table_insert(ht, weight, i)
+        item1 = hash_table_retrieve(ht, limit - weight)
+        if item1 is not None:
+            print(item1, "NOT NONE!", weight)
+            item2 = hash_table_retrieve(ht, weight)
+            print(item1, item2)
+            if item1 > item2:
+                return (item1, item2)
+            if item1 == item2:
+                return (1, 0)
+            else:
+                return (item2, item1)
     return None
 
 
